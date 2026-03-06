f10a3ea2ab9fb6285f4806fcaade3fd1b3ce42e3
SlytherPuff117 <lillybow2003@gmail.com>
2026-03-05 19:52:41 -0800

    Update ThirdPartyAdmin.cpp

--- src/backend/ThirdPartyAdmin.cpp
+++ src/backend/ThirdPartyAdmin.cpp
@@ -4,20 +4,20 @@ using namespace std;
 //started by Lilly Bowen on March 4, 2026
 //third party administrator class extends user class
 
-class ThirdPartyAdmin : public User{
+class ThirdPAdmin : public User{
     private:
-        string thirdPartyAdminID;
+        string thirdPAdminID;
 
     public:
-        string getThirdPartyAdminID() {
-            return thirdPartyAdminID;
+        string getThirdPAdminID() {
+            return thirdPAdminID;
         }
 
-        void setThirdPartyAdminID(string id) {
-            thirdPartyAdminID = id;
+        void setThirdPAdminID(string id) {
+            thirdPAdminID = id;
         }
 
-        void generateThirdPartyAssignment(){
+        void generateThirdPAssignment(){
             //use assignment class?
             //should have # of extinguishers, estimate of # of investigators, or just a list of inspectors/the company, locations of extinguishers, date it must be done by, other info as needed
             //send to investigators or third party company 
