d0a77aa98c3041896286bdaa205338e36e02c90d
SlytherPuff117 <lillybow2003@gmail.com>
2026-03-06 18:34:43 -0800

    Update Admin.cpp

--- src/backend/Admin.cpp
+++ src/backend/Admin.cpp
@@ -4,6 +4,15 @@ using namespace std;
 //started by Lilly Bowen on March 3, 2026
 //creates Admin class which extends User class
 
+//added by Lilly on March 6
+//requirements to be tested throughout process:
+//Admin object is created
+//functional getters & setters
+//ensure functions from parent class work
+//ensure db functions (access, change, view) work
+//ensure assignment generation and sending works\
+//ensure that the admin can view reports (not sure if this will be included here or in a different class)
+
 class Admin : public User{
     private:
         string adminID;
@@ -45,9 +54,11 @@ class Admin : public User{
             switch(change){
                 case 1:
                     //add extinguisher code
+                    cout << "The extinguisher has been added successfully!" << endl;
                     break;
                 case 2:
                     //remove extinguisher code
+                    cout << "The extinguisher has been removed successfully!" << endl;
                     break;
                 case 3:
                     //update extinguisher code
@@ -57,15 +68,19 @@ class Admin : public User{
                     switch(update){
                         case 1:
                             //update extinguisher type code
+                            cout << "The extinguisher type has been changed successfully!" << endl;
                             break;
                         case 2:
                             //update extinguisher location code
+                            cout << "The extinguisher location has been changed successfully!" << endl;
                             break;
                         case 3:
                             //update inspection interval code
+                            cout << "The extinguisher interval has been changed successfully!" << endl;
                             break;
                         case 4:
                             //add report code
+                            cout << "The extinguisher report has been added successfully!" << endl;
                             break;
                         default:
                             cout << "Invalid option. Please try again." << endl;
@@ -88,5 +103,6 @@ class Admin : public User{
             //use assignment class?
             //should have # of extinguishers, estimate of # of investigators, or just a list of inspectors/the company, locations of extinguishers, date it must be done by, other info as needed
             //send to investigators or third party company 
+            cout << "The report has be generated successfully!" << endl;
         }
 }
