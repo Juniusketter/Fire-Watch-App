3d7e879a95351e83b19b4d1e2e9c8e2a583ecaa1
SlytherPuff117 <lillybow2003@gmail.com>
2026-03-06 18:24:53 -0800

    Update User.cpp

--- src/backend/User.cpp
+++ src/backend/User.cpp
@@ -5,6 +5,12 @@ using namespace std;
 //creates user class that will be extended by Admin, Investigator, ThirdPartyAdmin, & ThirdPartyInvestigator
 //some of the text in this class & in others will be primarily for testing purposes & can/will be taken out later
 
+//added by Lilly on March 6
+//requirements to be tested throughout process:
+//User object is created
+//functional getters & setters
+//login and logout functions work, and will direct you to the appropriate page
+
 class User{
     private:
         string username;
@@ -12,6 +18,9 @@ class User{
         string companyName;
 
     public:
+
+        //maybe add parametized constructor to cut down on code length
+
         void setUsername(string u) {
             username = u;
         }
@@ -29,6 +38,7 @@ class User{
         }
         
         //UI could also have "forgot password" or "forgot username" areas that can be added if we choose to
+        //will need to be connected to the build-auth-zip.yml file, but that might need to be done through the main.cpp file that has not been created yet. 
         void login() {
             string u;
             string p;
