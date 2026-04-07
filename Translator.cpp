8c81377052055ee9378274b064b29612a93d4d6d
Quinci Walker <mrsqsjackson.78@gmail.com>
2026-04-07 17:47:18 -0500

    Add files via upload

--- Translator.cpp
+++ Translator.cpp
@@ -1,23 +1,21 @@
 #include "Translator.h"
 #include "TranslationStore.h"
-#include <QWidget>
 #include <QLabel>
-#include <QAction>
 #include <QAbstractButton>
+#include <QAction>
+#include <QWidget>
 
 void applyTranslations(QObject* root)
 {
     if (!root) return;
 
-    QVariant v = root->property("trKey");
-    if (v.isValid()) {
-        QString text = TranslationStore::translate(v.toString());
+    QVariant key = root->property("trKey");
+    if (key.isValid()) {
+        QString text = TranslationStore::t(key.toString());
         if (auto* l = qobject_cast<QLabel*>(root)) l->setText(text);
-        if (auto* b = qobject_cast<QAbstractButton*>(root)) b->setText(text);
-        if (auto* a = qobject_cast<QAction*>(root)) a->setText(text);
-        if (auto* w = qobject_cast<QWidget*>(root)) w->setWindowTitle(text);
+        else if (auto* b = qobject_cast<QAbstractButton*>(root)) b->setText(text);
+        else if (auto* a = qobject_cast<QAction*>(root)) a->setText(text);
+        else if (auto* w = qobject_cast<QWidget*>(root)) w->setWindowTitle(text);
     }
-
-    for (QObject* c : root->children())
-        applyTranslations(c);
+    for (QObject* c : root->children()) applyTranslations(c);
 }
